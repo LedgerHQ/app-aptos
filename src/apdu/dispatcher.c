70cebf69a37b86a795744df29885211f8f691c69
João Martins <joaomartinsfcup@gmail.com>
2025-03-28 12:43:25 +0000

    NAPPS-1110 Improving coverage for transaction utils.

--- src/apdu/dispatcher.c
+++ src/apdu/dispatcher.c
@@ -72,7 +72,7 @@ int apdu_dispatcher(const command_t *cmd) {
         case SIGN_TX:
             PRINTF("SIGN_TX\n");
             if ((cmd->p1 == P1_START && cmd->p2 != P2_MORE) ||  //
-                cmd->p1 > P1_MAX ||                             //
+                cmd->p1 >= P1_MAX ||                            //
                 (cmd->p2 != P2_LAST && cmd->p2 != P2_MORE)) {
                 PRINTF("Wrong P1 & P2 combo\n");
                 return io_send_sw(SW_WRONG_P1P2);
