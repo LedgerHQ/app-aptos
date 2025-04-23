a58525ac890c26932769e1e55d9a478d582d0127
João Martins <joaomartinsfcup@gmail.com>
2025-04-23 19:35:58 +0100

    Changing fuzzer to use static library instead of shared.

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -6,8 +6,6 @@ pushd fuzzing
 cmake -DBOLOS_SDK=../BOLOS_SDK -Bbuild -H.
 make -C build
 mv ./build/fuzz_tx_parser "${OUT}/fuzz_tx_parser"
-mv ./build/libtxparser.so "${OUT}/libtxparser.so"
-mv ./build/libtxparser.so.1 "${OUT}/libtxparser.so.1"
 pwd
 ls -l "${OUT}"
 popd
