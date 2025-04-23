f76e8f43be6b2a3bc8de2b2ba5a0561c383bd0ab
João Martins <joaomartinsfcup@gmail.com>
2025-04-23 17:30:37 +0100

    Some more fixes.

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -5,7 +5,8 @@
 pushd fuzzing
 cmake -DBOLOS_SDK=../BOLOS_SDK -Bbuild -H.
 make -C build
-mv ./build/fuzz_tx_parser "${OUT}"
-mv ./build/libtxparser.so "${OUT}"
-mv ./build/libtxparser.so.1 "${OUT}"
+mv ./build/fuzz_tx_parser "${OUT}/fuzz_tx_parser"
+mv ./build/libtxparser.so "${OUT}/libtxparser.so"
+mv ./build/libtxparser.so.1 "${OUT}/libtxparser.so.1"
+ls -l "${OUT}"
 popd
