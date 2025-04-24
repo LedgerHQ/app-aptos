16c7ae3c3cd1bd8263e39525a050a65d5f7fa1e9
João Martins <joaomartinsfcup@gmail.com>
2025-04-24 16:10:48 +0100

    Making slight changes to the build.

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -3,9 +3,7 @@
 # build fuzzers
 
 pushd fuzzing
-cmake -DBOLOS_SDK=../BOLOS_SDK -Bbuild -H.
-make -C build
+cmake -DBOLOS_SDK=../BOLOS_SDK -Bbuild -H. -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCODE_COVERAGE=1
+cmake --build build --target fuzz_tx_parser
 mv ./build/fuzz_tx_parser "${OUT}/fuzz_tx_parser"
-pwd
-ls -l "${OUT}"
 popd
