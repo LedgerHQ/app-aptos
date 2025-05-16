8f544f88190a1513b8b337a7e281d3d787fb8ba8
João Martins <joaomartinsfcup@gmail.com>
2025-05-16 11:31:32 +0100

    Fixes to the fuzzing.

--- fuzzing/run.sh
+++ fuzzing/run.sh
@@ -6,4 +6,7 @@ SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 BUILDDIR="$SCRIPTDIR/build"
 CORPUSDIR="$SCRIPTDIR/corpus"
 
+# Create corpus directory if it doesn't exist
+mkdir -p "$CORPUSDIR"
+
 "$BUILDDIR"/fuzz_tx_parser "$CORPUSDIR" "$@" >/dev/null
